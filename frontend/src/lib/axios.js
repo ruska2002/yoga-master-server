a2f071b4df05d3c58d881b64a6b73e1ea27d5a2f
Ruska Metskhovrishvili <r_metskhovrishvili@cu.edu.ge>
2026-03-12 18:34:13 +0400

    update api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:3000"
-    : "/";
+    : "https://yoga-master-server-zquu.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
